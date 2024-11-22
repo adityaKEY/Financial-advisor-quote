27ba91abe32372c7f2b9477fa52ce8030398b44a
adityaKEY <aditya.kumar@in.ey.com>
2024-11-22 15:11:24 +0530

    created get-lead-info api

--- config/routesValues.js
+++ config/routesValues.js
@@ -1,3 +1,4 @@
 exports.routeValues = {
   "/quote/fetch-quote-meta-data": "c2a04e70f16541f5b52f1b0803ee46d3",
+  "/quote/get-lead-info": "c2a04e70f16541f5b52f1b0803ee46d3",
 };
--- controllers/quote.js
+++ controllers/quote.js
@@ -1,48 +1,77 @@
 const { responseFormatter, statusCodes } = require("../utils");
-const { quote } = require("../db");
+const { event, quote, lead } = require("../db");
 
 exports.getMetaData = async (request, reply) => {
-    try {
-      const { metaMasters } = request.body;
-      const metaData = await quote.getMetaData(metaMasters); // Getting meta data from DB & maping keys   
+  try {
+    const { metaMasters } = request.body;
+    const metaData = await quote.getMetaData(metaMasters); // Getting meta data from DB & maping keys
 
-      if (metaData.length > 0) {
-        // await event.insertEventTransaction(request.isValid);
-        const groupedData = metaData.reduce((acc, item) => {
-          if (!acc[item.meta_master_name]) {
-            acc[item.meta_master_name] = [];
-          }
-          acc[item.meta_master_name].push(item);
-          return acc;
-        }, {});
-        return reply
-          .status(statusCodes.OK)
-          .send(
-            responseFormatter(
-              statusCodes.OK,
-              "Meta Data retrieved successfully",
-              groupedData
-            )
-          );
-      } else {
-        return reply
-          .status(statusCodes.NO_CONTENT)
-          .send(
-            responseFormatter(
-              statusCodes.NO_CONTENT,
-              "Data not found"
-            )
-          );
-      }
-    } catch (error) {
+    if (metaData.length > 0) {
+      await event.insertEventTransaction(request.isValid);
+      const groupedData = metaData.reduce((acc, item) => {
+        if (!acc[item.meta_master_name]) {
+          acc[item.meta_master_name] = [];
+        }
+        acc[item.meta_master_name].push(item);
+        return acc;
+      }, {});
       return reply
-        .status(statusCodes.INTERNAL_SERVER_ERROR)
+        .status(statusCodes.OK)
         .send(
           responseFormatter(
-            statusCodes.INTERNAL_SERVER_ERROR,
-            "Internal server error occurred",
-            { error: error.message }
+            statusCodes.OK,
+            "Meta Data retrieved successfully",
+            groupedData
           )
         );
+    } else {
+      return reply
+        .status(statusCodes.NO_CONTENT)
+        .send(responseFormatter(statusCodes.NO_CONTENT, "Data not found"));
     }
-}
+  } catch (error) {
+    return reply
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .send(
+        responseFormatter(
+          statusCodes.INTERNAL_SERVER_ERROR,
+          "Internal server error occurred",
+          { error: error.message }
+        )
+      );
+  }
+};
+
+exports.getLeadInfo = async (request, reply) => {
+  try {
+    const { idlead } = request.body;
+    const identity = request.isValid.identity;
+    const leadInfo = await lead.getLeadInfo(idlead, identity);
+    if (leadInfo.length) {
+      await event.insertEventTransaction(request.isValid);
+      return reply
+        .status(statusCodes.OK)
+        .send(
+          responseFormatter(
+            statusCodes.OK,
+            "Fetched Lead Info Successfully",
+            leadInfo[0]
+          )
+        );
+    } else {
+      return reply
+        .status(statusCodes.OK)
+        .send(responseFormatter(statusCodes.OK, "Lead Info Not Found"));
+    }
+  } catch (error) {
+    return reply
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .send(
+        responseFormatter(
+          statusCodes.INTERNAL_SERVER_ERROR,
+          "Internal server error occurred",
+          { error: error.message }
+        )
+      );
+  }
+};

c35e678168e3ebbcc17df81c0e943a6fef94d09b
adityaKEY <aditya.kumar@in.ey.com>
2024-11-26 16:55:59 +0530

    created getAddonRiders api

--- routes/quote.js
+++ routes/quote.js
@@ -1,23 +1,27 @@
 const { authentication, validation } = require("../middleware");
 const { quote } = require("../controllers");
 
-
 async function quoteRoutes(fastify, options) {
-    fastify.post(
-        "/fetch-quote-meta-data",
-        { preHandler: [authentication, validation] },
-        quote.getMetaData
-    );
-    fastify.post(
-        "/get-lead-info",
-        { preHandler: [authentication, validation] },
-        quote.getLeadInfo
-    );
-    fastify.post(
-        "/create-quote",
-        { preHandler: [authentication, validation] },
-        quote.createQuote
-    );
-  }
-  
-  module.exports = quoteRoutes;
+  fastify.post(
+    "/fetch-quote-meta-data",
+    { preHandler: [authentication, validation] },
+    quote.getMetaData
+  );
+  fastify.post(
+    "/get-lead-info",
+    { preHandler: [authentication, validation] },
+    quote.getLeadInfo
+  );
+  fastify.post(
+    "/create-quote",
+    { preHandler: [authentication, validation] },
+    quote.createQuote
+  );
+  fastify.post(
+    "/get-addon-premium",
+    { preHandler: [authentication, validation] },
+    quote.getAddOnPremium
+  );
+}
+
+module.exports = quoteRoutes;
